e55c337f861d3c577054781c362fdca98b58d0f3
Luka Noza <103760971+LukaNoza@users.noreply.github.com>
2023-11-09 20:24:27 +0400

    4

---
+++ main.js
@@ -0,0 +1,37 @@
+// 1
+// let multiplesOfThree = [];
+// for (let i = 0; i <= 100; i += 3) {
+//   multiplesOfThree.push(i);
+// }
+// console.log(multiplesOfThree);
+
+// 2
+// let friends = ["Alika", "luka", "Alexandre", "Dato", "Eka"];
+// let namesStartingWithA = friends.filter(name => name.startsWith("A"));
+// console.log(namesStartingWithA);
+
+// 3
+// let numbers = [];
+// for (let i = 0; i <= 50; i++) {
+//   if ((i % 2 === 0 || i % 5 === 0) && !(i % 2 === 0 && i % 5 === 0)) {
+//     numbers.push(i);
+//   }
+// }
+// console.log(numbers);
+
+// 4
+// let numbers = [2, 121, 4341, 242, 562, 43, 182, 55, 32, 98, 326];
+// let zeroCount = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   let currentNumber = numbers[i];
+//   let digitString = currentNumber.toString();
+  
+//   for (let j = 0; j < digitString.length; j++) {
+//     if (digitString[j] === '0') {
+//       zeroCount++;
+//     }
+//   }
+// }
+
+// console.log(zeroCount);
